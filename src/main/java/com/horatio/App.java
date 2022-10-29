a8867d9f1f1333b7e14e95c3f6245e99a7036407
Horatio Wilson <horatio.wilson@teamrockstars.nl>
2022-10-29 13:29:16 +0200

    Using perceptron to be able to make all the logic gates. These include AND, OR, NOR, XOR, NAND and XNOR.

--- src/main/java/com/horatio/App.java
+++ src/main/java/com/horatio/App.java
@@ -2,26 +2,46 @@ package com.horatio;
 
 public class App {
     public static void main(String[] args) {
+        for(int i = 0; i < 4; i++) {
+            double x1 = i/2;
+            double x2 = i%2;
+            double output = xnor(x1, x2);
+            System.out.printf("%d%d\t%d\n", (int)x1, (int)x2, (int)output);
+        }
+    }
 
-        // Inputs
-        double[] x = {0, 1};
-        // Weights
-        double[] w = {0.5, 0.5};
-        // Bias
-        double b = 0.5;
+    static double and(double x1, double x2) {
+        return neuron(new double[] {x1, x2}, new double[] {1, 1} , -1);
+    }
+
+    static double or(double x1, double x2) {
+        return neuron(new double[] {x1, x2}, new double[] {1, 1} , 0);
+    }
+
+    static double xor(double x1, double x2) {
+        return and(or(x1, x2), nand(x1, x2));
+    }
+
+    static double nor(double x1, double x2) {
+        return neuron(new double[] {x1, x2}, new double[] {-1, -1} , 1);
+    }
+
+    static double nand(double x1, double x2) {
+        return neuron(new double[] {x1, x2}, new double[] {-1, -1} , 2);
+    }
+
+    static double xnor(double x1, double x2) {
+        return or(and(x1, x2), nor(x1, x2));
+    }
+
+    static double neuron(double[] x, double[] w, double b) {
         // Weighted sum
         double z = 0.0;
-
         for(int i = 0; i < x.length; i++) {
             z += x[i] * w[i];
         }
-
         z += b;
-
         // Activation function
-        double a = z > 0 ? 1.0: 0.0;
-
-        // Print activation (output)
-        System.out.println(a);
+        return z > 0 ? 1.0: 0.0;
     }
 }
